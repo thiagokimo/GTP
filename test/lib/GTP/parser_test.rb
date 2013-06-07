e7ca447ef284eda4d6b82520a5ca1fd47e430d74
Thiago Rocha <kimobr@gmail.com>
2013-06-07 19:31:45 -0300

    Adding remaining empty tests

--- test/lib/GTP/parser_test.rb
+++ test/lib/GTP/parser_test.rb
@@ -7,4 +7,32 @@ describe GTP::Parser do
 
     parser.parse_version.must_equal "FICHIER GUITAR PRO v4.00"
   end
+
+  it "must read the tablature info" do
+    
+  end
+
+  it "must read the tablature lyrics" do
+    
+  end
+
+  it "must read the tablature extra info" do
+    
+  end
+
+  it "must read the tablature measures" do
+    
+  end
+
+  it "must read the tablature tracks" do
+    
+  end
+
+  it "must read the tablature song" do
+    
+  end
+
+  it "must read the tablature chord diagrams" do
+    
+  end
 end
