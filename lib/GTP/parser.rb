5b32fb3997cf1aee6538493d5b13a135f3a6edc5
Thiago Rocha <kimobr@gmail.com>
2013-06-07 19:27:06 -0300

    Parsing version

--- lib/GTP/parser.rb
+++ lib/GTP/parser.rb
@@ -1,5 +1,15 @@
 module GTP
   class Parser
 
+    attr_accessor :file, :version
+    attr_reader :file_path
+
+    def initialize(tab_path)
+      @file = File.open(tab_path, "r")
+    end
+
+    def parse_version
+      self.version = IO.binread(self.file, 30, 1).strip
+    end
   end
 end
--- test/lib/GTP/parser_test.rb
+++ test/lib/GTP/parser_test.rb
@@ -2,6 +2,9 @@ require "test_helper"
 
 describe GTP::Parser do
 
-  it "must open a GTP file" do
+  it "must read the file version" do
+    parser = GTP::Parser.new "test/tabs/test.gp4"
+
+    parser.parse_version.must_equal "FICHIER GUITAR PRO v4.00"
   end
 end
