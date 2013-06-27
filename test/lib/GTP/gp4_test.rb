6acf762bb7dd5196a8601145fd0a9a9bda3e08fa
Lukas Alexandre <lukasalexandre@me.com>
2013-06-26 21:05:18 -0400

    Refactoring the test module

--- test/lib/GTP/gp4_test.rb
+++ test/lib/GTP/gp4_test.rb
@@ -1,53 +1,55 @@
 require "test_helper"
 
-describe GTP::GP4 do
+module GTP
+  describe GP4 do
 
-  it "must read the file version" do
-    parser = GTP::GP4.new "test/tabs/test.gp4"
+    it "must read the file version" do
+      parser = GP4.new "test/tabs/test.gp4"
 
-    parser.parse_version
+      parser.parse_version
 
-    parser.version.must_match %r/FICHIER GUITAR PRO v4./
-  end
+      parser.version.must_match %r/FICHIER GUITAR PRO v4./
+    end
 
-  it "must read the tablature info" do
-    parser = GTP::GP4.new "test/tabs/test.gp4"
+    it "must read the tablature info" do
+      parser = GP4.new "test/tabs/test.gp4"
 
-    parser.parse_info
+      parser.parse_info
 
-    parser.title.must_equal "Title"
-    parser.subtitle.must_equal "Subtitle"
-    parser.artist.must_equal "Artist"
-    parser.album.must_equal "Album"
-    parser.author.must_equal "Author"
-    parser.copyright.must_equal "Copyright"
-    parser.tab.must_equal "Tab"
-    parser.instruction.must_equal "Instruction"
-    parser.notice.must_equal "N line 1\nN line 2\nN line 3\nN line 4"
-    parser.triplet_feel.must_match "0"
-  end
+      parser.title.must_equal "Title"
+      parser.subtitle.must_equal "Subtitle"
+      parser.artist.must_equal "Artist"
+      parser.album.must_equal "Album"
+      parser.author.must_equal "Author"
+      parser.copyright.must_equal "Copyright"
+      parser.tab.must_equal "Tab"
+      parser.instruction.must_equal "Instruction"
+      parser.notice.must_equal "N line 1\nN line 2\nN line 3\nN line 4"
+      parser.triplet_feel.must_match "0"
+    end
 
-  it "must read the tablature lyrics" do
-    
-  end
+    it "must read the tablature lyrics" do
 
-  it "must read the tablature extra info" do
-    
-  end
+    end
 
-  it "must read the tablature measures" do
-    
-  end
+    it "must read the tablature extra info" do
 
-  it "must read the tablature tracks" do
-    
-  end
+    end
 
-  it "must read the tablature song" do
-    
-  end
+    it "must read the tablature measures" do
 
-  it "must read the tablature chord diagrams" do
-    
+    end
+
+    it "must read the tablature tracks" do
+
+    end
+
+    it "must read the tablature song" do
+
+    end
+
+    it "must read the tablature chord diagrams" do
+
+    end
   end
 end
